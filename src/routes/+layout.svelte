b64d1c73a0f091e5c626fd2fb6ea109e522b5b1b
nastya <adequateuser228@gmail.com>
2024-03-17 16:43:27 +0300

    add finish

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<nav>
+    <div class = "nav-container">
+        <a href="/" class ="nav-logo" title="Back to Homepages">Настя Палагина</a>
+        <div class="nav-links">
+            {#each nav as link}
+                {#if link.isShow}
+                    <a href={link.href} class="link">{link.title}</a>
+                    <span>&nbsp;&nbsp;&nbsp;</span>
+                {/if}
+            {/each}
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <slot></slot>
+</div>
+<script>
+    const nav = [
+        {
+         title: "Виды программистов",
+         href: "/about",
+         isShow: true,
+        },
+        {
+         title: "Обучение",
+         href: "/blog",
+         isShow: true,
+        },
+        {
+         title: "Перспективы",
+         href: "/project",
+         isShow: true,
+        },
+        {
+         title: "Итог",
+         href: "/contact",
+         isShow: true,
+        },
+        {
+         title: "temp",
+         href: "/temp",
+         isShow: false,
+        },
+    ];
+</script>
+<style>
+    .container{
+        max-width: 1400px;
+        margin: 50 px auto;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,86 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+<title>Палагина Настя</title>
+</svelte:head>
+<body>
+<div class ="container">
+    <div class="info">
+        <div class = "animate-background">
+            <div class="container info-container">
+                <div class="info-text">
+                    <h1>
+                        <p class="animate-text-1">Профессия "программист",</p>
+                        <p class="text-2">Виды, обучение и перспективы.</p>
+                    </h1>
+                </div>
+                <h2 class="subtitle">
+                    Я думаю вам будет интересно почитать этот сайт, может найдете что-то новенькое для себя.
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="background-div"></div>
+</body>
+<style>
+    body{
+        margin:auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-height: 100%;
+        padding: 5vmin;
+        box-sizing: border-box;
+        background: antiquewhite;
+    }
+    .info {
+        text-align: center;
+        position: relative;
+    }
+
+    .info-container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: left;
+        align-items: start;
+    }
+
+    .container {
+        max-width: 1400px;
+        margin: 190px 0 0 2.5rem;
+    }
+
+    .info h1 {
+        font-size: 4rem;
+        margin: 0;
+    }
+
+    .info h1 p {
+        position: relative;
+        font: inherit;
+        margin: 0;
+        text-align: left;
+        line-height: 130%;
+        width: fit-content;
+        font-weight: 800;
+        letter-spacing: -0.03em;
+    }
+
+    .info h2 {
+        max-width: 65ch;
+        font-weight: 200;
+        margin-top: 0.50rem;
+    }
+    
+    .background-div {
+        background-color: #ddc98f;
+        width: 100%;
+        height: 56vh;
+        margin: -205px auto;
+    }
+    .info .info-text{
+        position: relative;
+    }
+</style>
